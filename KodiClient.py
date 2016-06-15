7c612b9ccabd3b4bd599659af86587fcf1e3229e
CharlesSchimmel <charlesschimmel@gmail.com>
2016-06-15 20:36:09 +0000

    Objectification!

---
+++ KodiClient.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+import requests
+
+class Kodi:
+    """
+    Contains methods relating to interacting with a Kodi instance's JSON server.
+    Note: Kodi doesn't respond with typical HTTP error codes.
+    """
+
+    def __init__(self):
+        self.host = "localhost"
+        self.port = 8080
+        self.headers = {'Content-Type': 'application/json'}
+
+    def inputAction(self,action):
+        """
+        Sends the given action specified by: http://kodi.wiki/view/JSON-RPC_API/v6#Input.Action
+        Left, Right, Up, Down, Back, Pause, etc.
+        Returns the server's JSON response.
+        """
+        payload = '{"id": "1", "jsonrpc": "2.0", "method": "Input.ExecuteAction","params":{"action":"'+action+'"}}'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        return r.json()
+
+    def updateAV(self,library):
+        """
+        Given the specified library type (VideoLibrary or AudioLibrary), asks the kodi server to update it.
+        Returns the server's JSON response.
+        """
+        payload = '{"id": "1", "jsonrpc": "2.0", "method": "'+library+'.Scan"}}'
+        r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=self.headers)
+        return r.json()
+
+    def sendText(self,text):
+        """
+        Takes a given string and sends it as input.
+        Returns the server's JSON response.
+        """
+        payload = '{"id": "1", "jsonrpc": "2.0", "method": "Input.SendText","params":{"text":"'+text+'"}}'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        return(r.json())
+
+    def getWindowID(self):
+        """
+        Queries the server and returns the currently active window number.
+        """
+        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "GUI.GetProperties", "params":{"properties":["currentwindow"]} }'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        return r.json()['result']['currentwindow']['id']
+
+    def getProperties(self):
+        """
+        Queries the server and returns the currently active players properties.
+        """
+        playerid = self.getPlayerID()
+        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.GetProperties", "params":{"playerid":'+playerid+',"properties":["playlistid","time","totaltime","percentage","type","speed"]} }'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        return r.json()
+
+
+    def getPlayerID(self):
+        """
+        Queries the server and returns the player id as an integer.
+        If there's none to be found (ie nothing is playing) returns false.
+        """
+        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.GetActivePlayers" }'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        try:
+            return str(r.json()['result'][0]['playerid'])
+        except (IndexError):
+            return False
+
+    def getTitle(self,playerid=False):
+        """
+        Ideally returns the title,artist(or showtitle) of the currently playing media.
+        Returns False,False if such information isn't available (ex: pictures, obscure plugins.)
+        Will get playerid itself if not defined. Recommended to define playerid to limit number of requests.
+        """
+        if playerid == False:
+            playerid = self.getPlayerID()
+        try:
+            payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.GetItem", "params":{"playerid":'+playerid+',"properties":["title","artist","showtitle"]} }'
+            r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+            title = r.json()['result']['item']['title']
+            if 'showtitle' not in r.json()['result']['item']:
+                artist = r.json()['result']['item']['artist'][0]
+            else:
+                artist = r.json()['result']['item']['showtitle']
+            return title,artist
+        except:
+            return False,False
+
+    def getFormattedTimes(self,curProperties=False):
+        if curProperties == False:
+            curProperties = self.getProperties()
+        try:
+            if curProperties['result']['totaltime']['hours'] == 0:
+                curTime =  "{}:{:02d}".format(curProperties['result']['time']['minutes'],curProperties['result']['time']['seconds'])
+                totalTime = "{}:{:02d}".format(curProperties['result']['totaltime']['minutes'],curProperties['result']['totaltime']['seconds'])
+            else:
+                curTime =  "{}:{:02d}:{:02d}".format(curProperties['result']['time']['hours'],curProperties['result']['time']['minutes'],curProperties['result']['time']['seconds'])
+                totalTime = "{}:{:02d}:{:02d}".format(curProperties['result']['totaltime']['hours'],curProperties['result']['totaltime']['minutes'],curProperties['result']['totaltime']['seconds'])
+        except:
+            return "00:00/00:00"
+        return curTime,totalTime
+
+
+    def getRecentEps(self):
+        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "VideoLibrary.GetRecentlyAddedEpisodes" }'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        try:
+            return r.json()['results']['episodes']
+        except (IndexError,KeyError):
+            return False
+
+    def getPlaylistItems(self,curProperties=False):
+        if curProperties == False:
+            playlistid = str(self.getProperties()['result']['playlistid'])
+        else:
+            playlistid = str(curProperties['result']['playlistid'])
+        try:
+            payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Playlist.GetItems", "params":{"playlistid":'+playlistid+',"properties":["title"]} }'
+            r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+            return r.json()['result']['items']
+        except:
+            return False
+
+    def getEpDetails(self,episodeid):
+        episodeid = str(episodeid)
+        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodeDetails", "params":{"episodeid":'+episodeid+',"properties":["file"]} }'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        return r.json()['result']['episodedetails']
+
+    def openFile(self,targetFile):
+        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"file":"'+targetFile+'"}}}'
+        r = requests.post("http://{}:{}/jsonrpc".format(self.host,self.port), data=payload, headers=self.headers)
+        return r.json()
--- kodiremote.py
+++ kodiremote.py
@@ -3,202 +3,101 @@
 # imports
 from blessed import Terminal
 import sys
-import requests
 import configparser
 from os import getenv
 from os import path
 from time import sleep, time
+from KodiClient import Kodi
 # import logging
 
+class Views:
+    
+    def __init__(self):
+        kodi = Kodi()
 
-def inputAction(action):
-    payload = '{"id": "1", "jsonrpc": "2.0", "method": "Input.ExecuteAction","params":{"action":"'+action+'"}}'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    return r.json()
-
-
-def updateAV(library):
-    payload = '{"id": "1", "jsonrpc": "2.0", "method": "'+library+'.Scan"}}'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    if r.status_code != 200:
-        print(r.text)
-
-
-def sendText(text):
-    payload = '{"id": "1", "jsonrpc": "2.0", "method": "Input.SendText","params":{"text":"'+text+'"}}'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    if r.status_code != 200:
-        print(r.text)
-
-
-def getWindowID():
-    payload = '{ "id": "1", "jsonrpc": "2.0", "method": "GUI.GetProperties", "params":{"properties":["currentwindow"]} }'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    if r.status_code != 200:
-        print(r.text)
-    return r.json()['result']['currentwindow']['id']
-
-
-
-def getProperties(playerid):
-    payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.GetProperties", "params":{"playerid":'+playerid+',"properties":["time","totaltime","percentage","type","speed"]} }'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    if r.status_code != 200:
-        print(r.text)
-    return r.json()
-
-
-
-def getPlayerID():
-    payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.GetActivePlayers" }'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    try:
-        return str(r.json()['result'][0]['playerid'])
-    except (IndexError):
-        return False
-
-
-def getTitle(playerid):
-    try:
-        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.GetItem", "params":{"playerid":'+playerid+',"properties":["title","artist","showtitle"]} }'
-        r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-        title = r.json()['result']['item']['title']
-        if 'showtitle' not in r.json()['result']['item']:
-            artist = r.json()['result']['item']['artist'][0]
-        else:
-            artist = r.json()['result']['item']['showtitle']
-        return title,artist
-    except:
-        return "null","null"
-
-
-def getTimes(curProperties):
-    try:
-        if curProperties['result']['totaltime']['hours'] == 0:
-            curTime =  "{}:{:02d}".format(curProperties['result']['time']['minutes'],curProperties['result']['time']['seconds'])
-            totalTime = "{}:{:02d}".format(curProperties['result']['totaltime']['minutes'],curProperties['result']['totaltime']['seconds'])
-        else:
-            curTime =  "{}:{}:{:02d}".format(curProperties['result']['time']['hours'],curProperties['result']['time']['minutes'],curProperties['result']['time']['seconds'])
-            totalTime = "{}:{}:{:02d}".format(curProperties['result']['totaltime']['hours'],curProperties['result']['totaltime']['minutes'],curProperties['result']['totaltime']['seconds'])
-        times = "{}/{}".format(curTime,totalTime)
-    except:
-        return "00:00/00:00"
-    return times
-
-
-def getRecentEps():
-    payload = '{ "id": "1", "jsonrpc": "2.0", "method": "VideoLibrary.GetRecentlyAddedEpisodes" }'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    try:
-        return r.json()
-    except (IndexError):
-        return False
-
-def getPlaylistItem():
-    try:
-        payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Playlist.GetItems", "params":{"playlistid":0,"properties":["title"]} }'
-        r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-        toReturn = r.json()['result']['items']
-    except:
-        try:
-            payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Playlist.GetItems", "params":{"playlistid":1, "properties":["title"]} }'
-            r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-            toReturn = r.json()['result']['items']
-        except:
-            payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Playlist.GetItems", "params":{"playlistid":2, "properties":["title"]} }'
-            r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-            toReturn = r.json()['result']['items']
-    return toReturn
-
-def getEpDetails(episodeid):
-    episodeid = str(episodeid)
-    payload = '{ "id": "1", "jsonrpc": "2.0", "method": "VideoLibrary.GetEpisodeDetails", "params":{"episodeid":'+episodeid+',"properties":["file"]} }'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    return r.json()['result']['episodedetails']
-
-def openFile(targetFile):
-    payload = '{ "id": "1", "jsonrpc": "2.0", "method": "Player.Open", "params":{"item":{"file":"'+targetFile+'"}}}'
-    r = requests.post("http://{}:{}/jsonrpc".format(kodiHost,kodiPort), data=payload, headers=headers)
-    return r.json()
-
-def keyParse(keyIn):
+def keyParse(keyIn,windowID,kodi):
     if keyIn == 'q':
         print(t.exit_fullscreen())
         sys.exit(0)
 
     elif keyIn.name == 'KEY_F1':
-        print(t.exit_fullscreen())
         helpView()
-    
+    elif keyIn.name == 'KEY_F2':
+        recentEpsMenu(kodi,t)
     elif keyIn.name == 'KEY_F5':
         with t.location(y=6):
             print(t.clear())
 
-    elif keyIn.name == 'KEY_F2':
-        recentEpsList = getRecentEps()['result']['episodes']
+
+    # Actions
+    elif keyIn.name == 'KEY_ESCAPE' or keyIn.name == "KEY_F11":
+        kodi.inputAction("fullscreen")
+    elif keyIn == ' ':
+        kodi.inputAction("pause")
+    elif keyIn == ']':
+        kodi.inputAction("skipnext")
+    elif keyIn == '[':
+        kodi.inputAction("skipprevious")
+    elif keyIn == 'x':
+        kodi.inputAction("stop")
+
+    elif keyIn == 'H':
+        kodi.inputAction("back")
+    elif keyIn == 'h':
+        if windowID == 12005 or windowID == 12006:
+            kodi.inputAction("stepback")
+        kodi.inputAction("left")
+    elif keyIn == 'l':
+        if windowID == 12005 or windowID == 12006:
+            kodi.inputAction("stepforward")
+        kodi.inputAction("right")
+    elif keyIn == 'k':
+        kodi.inputAction("up")
+    elif keyIn == 'j':
+        kodi.inputAction("down")
+    
+    elif keyIn == 'c':
+        kodi.inputAction("contextmenu")
+    elif keyIn == 'i':
+        kodi.inputAction("info")
+
+    elif keyIn == '-':
+        kodi.inputAction("volumedown")
+    elif keyIn == '=':
+        kodi.inputAction("volumeup")
+    elif keyIn == '0':
+        kodi.inputAction("mute")
+    
+    elif keyIn == 'u':
+        kodi.updateAV("VideoLibrary")
+    elif keyIn == 'U':
+        kodi.updateAV("AudioLibrary")
+
+    elif keyIn == 'u':
+        kodi.updateAV("VideoLibrary")
+    elif keyIn == 'U':
+        kodi.updateAV("AudioLibrary")
+
+
+    elif keyIn.name == 'KEY_ENTER':
+        if windowID != 10120:
+            kodi.inputAction("select")
+            kodi.inputAction("osd")
+        else:
+            kodi.inputAction("select")
+
+
+def recentEpsMenu(kodi,t):
+    recentEpsList = kodi.getRecentEps()
+    if recentEpsList != False:
         recentEpsInfo = [ getEpDetails(x['episodeid']) for x in recentEpsList]
         recentEpsList = [ x['label'] for x in recentEpsInfo]
         selectionLabel,selectionIndex = menuView(recentEpsList,t)
         if selectionLabel != False and selectionIndex != False:
             openFile(recentEpsInfo[selectionIndex]['file'])
-
-    # Actions
-    elif keyIn.name == 'KEY_ESCAPE' or keyIn.name == "KEY_F11":
-        inputAction("fullscreen")
-    elif keyIn == ' ':
-        inputAction("pause")
-    elif keyIn == ']':
-        inputAction("skipnext")
-    elif keyIn == '[':
-        inputAction("skipprevious")
-    elif keyIn == 'x':
-        inputAction("stop")
-
-    elif keyIn == 'H':
-        inputAction("back")
-    elif keyIn == 'h':
-        if getWindowID() == 12005 or getWindowID() == 12006:
-            inputAction("stepback")
-        inputAction("left")
-    elif keyIn == 'l':
-        if getWindowID() == 12005 or getWindowID() == 12006:
-            inputAction("stepforward")
-        inputAction("right")
-    elif keyIn == 'k':
-        inputAction("up")
-    elif keyIn == 'j':
-        inputAction("down")
-    
-    elif keyIn == 'c':
-        inputAction("contextmenu")
-    elif keyIn == 'i':
-        inputAction("info")
-
-    elif keyIn == '-':
-        inputAction("volumedown")
-    elif keyIn == '=':
-        inputAction("volumeup")
-    elif keyIn == '0':
-        inputAction("mute")
-    
-    elif keyIn == 'u':
-        updateAV("VideoLibrary")
-    elif keyIn == 'U':
-        updateAV("AudioLibrary")
-
-    elif keyIn == 'u':
-        updateAV("VideoLibrary")
-    elif keyIn == 'U':
-        updateAV("AudioLibrary")
-
-
-    elif keyIn.name == 'KEY_ENTER':
-        if getWindowID() != 10120:
-            inputAction("select")
-            inputAction("osd")
-        else:
-            inputAction("select")
-
+    else:
+        with t.location(y=0):
+            print(t.bold(t.center("No Recent Episodes")))
 
 def textPrompt(t):
     with t.location(x=0,y=5):
@@ -210,15 +109,23 @@ def textPrompt(t):
     return usrIn
 
 
-def nowPlayingView():
+def nowPlayingView(kodi):
     print(t.enter_fullscreen(),t.clear())
     with t.location(y=0,x=0):
         print(t.center(t.cyan(t.bold("Kodi Terminal Remote"))))
     while True:
+
         print(t.move(0,0))
+
+        playerid = kodi.getPlayerID()
+        windowID = kodi.getWindowID()
+
+        if windowID == 10103:
+            kodi.sendText(textPrompt(t))
+
         with t.cbreak(): 
             keyIn = t.inkey(1)
-            keyParse(keyIn)
+            keyParse(keyIn,windowID,kodi)
 
         if int(time()) % 10 == 0:
             # "Flush" the padding areas and title every few seconds.
@@ -231,21 +138,18 @@ def nowPlayingView():
                 print(t.center(""))
 
 
-        playerid = getPlayerID()
+        # If we get something for the playerid, something's playing.
         if playerid != False:
-            curProperties = getProperties(playerid)
-            times = getTimes(curProperties)
-            title,artist = getTitle(playerid)
+            curProperties = kodi.getProperties()
+            times = "{}/{}".format(kodi.getFormattedTimes())
+            title,artist = kodi.getTitle(playerid)
+
             progPerct = curProperties['result']['percentage']/100
             progWidth = t.width - len(times)
             progBar = int(progPerct*progWidth)
-            if displayPlaylist in ['true','True']:
-                if int(time()) % 2 == 0:
-                    try:
-                        playlistView(title)
-                    except:
-                        pass
 
+            if displayPlaylist.lower() in ['true','yes']:
+                playlistModule(kodi,t,title)
 
             with t.location(y=2):
                 if artist != '':
@@ -269,46 +173,44 @@ def nowPlayingView():
             with t.location(x=0, y=2):
                 print(t.center(t.bold("Play something!")))
 
-        if getWindowID() == 10103:
-            sendText(textPrompt(t))
 
+def playlistModule(kodi,t,title):
+    if int(time()) % 2 == 0:
+        try:
+            playlist = kodi.getPlaylistItems()
+            playlistItems = [ x['title'] for x in playlist ]
+            if len(playlistItems) > 1:
+                if playlistItems.index(title) > 2:
+                    # If we're a couple songs deep, We don't want to see the whole start of the playlist, just 2 before current
+                    playNum = playlistItems.index(title) - 2
+                    playlist = playlistItems[playNum:]
 
-def playlistView(title):
-    playlist = getPlaylistItem()
-    playlistItems = [ x['title'] for x in playlist ]
-    if len(playlistItems) > 1:
-        if playlistItems.index(title) > 2:
-            # If we're a couple songs deep, We don't want to see the whole start of the playlist, just 2 before current
-            playNum = playlistItems.index(title) - 2
-            playlist = playlistItems[playNum:]
-            # playlist = playlist[:playNum]
-            # playlist = playlistItems
+                    # Make "entries" into the list so that we flush old entries when it scrolls up
+                    # for i in range(len(playlist)+6,t.height):
+                    #     playlist.append(" "*t.width)
 
-            # Make "entries" into the list so that we flush old entries when it scrolls up
-            # for i in range(len(playlist)+6,t.height):
-            #     playlist.append(" "*t.width)
+                    for i in playlist[:t.height-7]:
+                        with t.location(y=6+playlist.index(i)):
+                            if i == title:
+                                print(t.center(t.bold(t.white_on_black(i))))
+                            else:
+                                print(t.center(i))
+                else:
+                    playlist = playlistItems
+                    # playlist = playlist[:playNum]
 
-            for i in playlist[:t.height-7]:
-                with t.location(y=6+playlist.index(i)):
-                    if i == title:
-                        print(t.center(t.bold(t.white_on_black(i))))
-                    else:
-                        print(t.center(i))
-        else:
-            playlist = playlistItems
-            # playlist = playlist[:playNum]
-            # playlist = playlistItems
+                    # Make "entries" into the list so that we flush old entries when it scrolls up
+                    # for i in range(len(playlist)+6,t.height):
+                    #     playlist.append(" "*t.width)
 
-            # Make "entries" into the list so that we flush old entries when it scrolls up
-            # for i in range(len(playlist)+6,t.height):
-            #     playlist.append(" "*t.width)
-
-            for i in playlist[:t.height-7]:
-                with t.location(y=6+playlist.index(i)):
-                    if i == title:
-                        print(t.center(t.bold(i)))
-                    else:
-                        print(t.center(i))
+                    for i in playlist[:t.height-7]:
+                        with t.location(y=6+playlist.index(i)):
+                            if i == title:
+                                print(t.center(t.bold(i)))
+                            else:
+                                print(t.center(i))
+        except:
+            pass
 
 def menuView(options,term):
     offset=2
@@ -359,39 +261,47 @@ def helpView():
                 break
 
 
-home = getenv("HOME")
-config = configparser.ConfigParser() #initializing config parser object
-config.read('{}/.kodiremote/kodiremote.ini'.format(home)) #sending config file to config parser object
-kodiHost = config['settings']['host']
-kodiPort = config['settings']['port']
-displayPlaylist = config['settings']['playlist']
-headers = {'Content-Type': 'application/json'}
-
 try:
+    config = configparser.ConfigParser() #initializing config parser object
+    config.read('{}/.kodiremote/kodiremote.ini'.format(getenv("HOME"))) #sending config file to config parser object
+    displayPlaylist = config['settings']['playlist']
+    kodi = Kodi()
+    kodi.host = config['settings']['host'] 
+    kodi.port = config['settings']['port']
     args = sys.argv[1:]
     if len(args) == 0:
         t = Terminal()
         with t.hidden_cursor(): 
-            nowPlayingView()
+            nowPlayingView(kodi)
 
-    elif len(args) >= 1:
-        if 'h' in args or 'help' in args or '--help' in args or '-help' in args:
-            print("Navigation is hjkl, H for back, enter to select. F1 for help screen.")
-            print("Run on its own or in one-shot mode with input action arguments.")
+    if len(args) >= 1:
+        # Checks if an intersection of two lists yields any results.
+        if bool(set(['h','H','help','--help','-help']) & set(args)):
+            print("Run on its own or in one-shot mode with 'action' argument followed by any number of actions.")
             print("Useful ones include left,right,down,up,pause,skipnext,skipprevious.")
             print("You can find all valid input action arguments here:")
             print("http://kodi.wiki/view/JSON-RPC_API/v6#Input.Action")
-            recentEpsList = getRecentEps()['result']['episodes']
-            recentEpsInfo = [ getEpDetails(x['episodeid']) for x in recentEpsList]
-            recentEpsList = [ x['label'] for x in recentEpsInfo]
-            t = Terminal()
-            print(menuView(recentEpsList,t))
-            print(recentEpsInfo)
-        else:
+            print("valid arguments: [host $HOST] [port $PORT]")
+            sys.exit(0)
+
+        if 'host' in args:
+            kodi.host = args[args.index('host') + 1]
+
+        if 'port' in args:
+            kodi.port = args[args.index('port') + 1]
+
+        if args[0] == 'action':
             for i in args:
-                print(inputAction(i))
+                print(kodi.inputAction(i))
+            sys.exit(0)
+
+        if 't' in args:
+            sys.exit(0)
+
+        t = Terminal()
+        with t.hidden_cursor(): 
+            nowPlayingView(kodi)
 
 except (KeyboardInterrupt,OSError,ConnectionError):
     print(t.exit_fullscreen())
     print("Cannot connect to Kodi server. Is it running? Is your config file configured?")
-    pass
